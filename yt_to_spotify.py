66e69fc0a399c1b607c2ebf2a70272fcb9ce0b50
Andrea Vorametsanti <avora@Andreas-MacBook-Pro.local>
2020-06-05 14:56:42 -0700

    retrieved playlist id

--- yt_to_spotify.py
+++ yt_to_spotify.py
@@ -1,3 +1,23 @@
 # Retrieve YT playlist songs
+    # Grab ID from URL
+    # Get request to API
+    # Save in JSON
 # Create Spotify playlist
 # Add each song in YT playlist to Spotify playlist
+
+from urllib.parse import urlparse
+
+def get_listID(url):
+    try:
+        parsed_URL = urlparse(playlist_URL)
+        parsed_params = parsed_URL.query.split('&')
+        query_obj = dict(query.split('=') for query in parsed_params)
+        return query_obj["list"]
+    except:
+        print("Invalid URL. Please copy and paste the URL of the playlist directly from Youtube.")
+
+
+
+playlist_URL = input("What's the link to the playlist?\n")
+print(get_listID(playlist_URL))
+
