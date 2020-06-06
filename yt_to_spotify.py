5f46833af044413d4551b24b059864c5d11ce3b2
Andrea Vorametsanti <avora@Andreas-MacBook-Pro.local>
2020-06-06 16:32:41 -0700

    retrieved playlist items

--- yt_to_spotify.py
+++ yt_to_spotify.py
@@ -5,10 +5,17 @@
 # Create Spotify playlist
 # Add each song in YT playlist to Spotify playlist
 
-from urllib.parse import urlparse
+import secrets
 import sys
+import os
+import requests
+from urllib.parse import urlparse
+
+
+YT_URL = "https://www.googleapis.com/youtube/v3/playlistItems"
 
 class generatePlaylist:
+
     # generates Spotify playist given Youtube playlist link
     def get_listID(self, url_input):
         try:
@@ -17,19 +24,30 @@ class generatePlaylist:
             if parsed_params[0]:
                 query_obj = dict(query.split('=') for query in parsed_params)
                 if query_obj["list"]:
-                    return query_obj["list"]
+                    self.yt_id = query_obj["list"]
+                    return
             else:
                 raise ValueError('Invalid URL. Please copy and paste the playlist URL from Youtube')
         except Exception as error:
             print(error)
             sys.exit(1)
 
+    def get_yt_videos(self):
+        response = requests.get(url = YT_URL, params = {
+            "part": "snippet",
+            "playlistId": self.yt_id,
+            "key": secrets.yt_key
+        })
+        print(response.text)
+        
+
+
 
 def main():
     ytTospotify = generatePlaylist()
     playlist_URL = input("What's the link to the playlist?\n")
-    id = ytTospotify.get_listID(playlist_URL)
-    print(id, "Well done")
+    ytTospotify.get_listID(playlist_URL)
+    ytTospotify.get_yt_videos()
 
 if __name__ == "__main__":
     main()
