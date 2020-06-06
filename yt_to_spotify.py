9db9f098c4a6ce070250fcbf87b81771d3bcc45b
Andrea Vorametsanti <avora@Andreas-MacBook-Pro.local>
2020-06-05 23:23:44 -0700

    debugged retrieving playlist URL

--- yt_to_spotify.py
+++ yt_to_spotify.py
@@ -6,18 +6,31 @@
 # Add each song in YT playlist to Spotify playlist
 
 from urllib.parse import urlparse
+import sys
 
-def get_listID(url):
-    try:
-        parsed_URL = urlparse(playlist_URL)
-        parsed_params = parsed_URL.query.split('&')
-        query_obj = dict(query.split('=') for query in parsed_params)
-        return query_obj["list"]
-    except:
-        print("Invalid URL. Please copy and paste the URL of the playlist directly from Youtube.")
+class generatePlaylist:
+    # generates Spotify playist given Youtube playlist link
+    def get_listID(self, url_input):
+        try:
+            parsed_URL = urlparse(url_input)
+            parsed_params = parsed_URL.query.split('&')
+            if parsed_params[0]:
+                query_obj = dict(query.split('=') for query in parsed_params)
+                if query_obj["list"]:
+                    return query_obj["list"]
+            else:
+                raise ValueError('Invalid URL. Please copy and paste the playlist URL from Youtube')
+        except Exception as error:
+            print(error)
+            sys.exit(1)
 
 
+def main():
+    ytTospotify = generatePlaylist()
+    playlist_URL = input("What's the link to the playlist?\n")
+    id = ytTospotify.get_listID(playlist_URL)
+    print(id, "Well done")
 
-playlist_URL = input("What's the link to the playlist?\n")
-print(get_listID(playlist_URL))
+if __name__ == "__main__":
+    main()
 
